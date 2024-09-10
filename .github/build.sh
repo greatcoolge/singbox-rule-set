75ce2c2aa3b3c2de2acaee869a4c2164b10cb92e
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2024-09-10 09:58:18 +0800

    build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -2,11 +2,16 @@
 
 set -e -o pipefail
 
-VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest \
-| grep tag_name \
-| cut -d ":" -f2 \
-| sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+# 输出当前工作目录
+echo "当前工作目录：$(pwd)"
 
+# 打印下载的文件是否存在
+echo "检查下载的文件是否存在..."
+ls -la sing-box.tar.gz geoip.db geosite.db
+
+# 执行下载和解压命令
+echo "下载和解压文件..."
+VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest | grep tag_name | cut -d ":" -f2 | sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
 curl -Lo sing-box.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box-${VERSION}-linux-amd64.tar.gz"
 curl -Lo geoip.db "https://github.com/lyc8503/sing-box-rules/releases/latest/download/geoip.db"
 curl -Lo geosite.db "https://github.com/lyc8503/sing-box-rules/releases/latest/download/geosite.db"
@@ -15,6 +20,12 @@ tar -xzvf sing-box.tar.gz
 mv ./sing-box-${VERSION}-linux-amd64/sing-box .
 chmod +x sing-box
 
+# 打印文件夹和文件列表
+echo "打印文件夹和文件列表..."
+ls -la
+
+# 执行导出和编译命令
+echo "导出和编译规则..."
 geoipAddresses=("cn" "de" "facebook" "google" "netflix" "telegram" "twitter")
 geositeDomains=("amazon" "apple" "bilibili" "category-ads-all" "category-games" "cn" "discord" "disney" "facebook" "geolocation-!cn" "github" "google" "instagram" "microsoft" "netflix" "onedrive" "openai" "primevideo" "telegram" "tiktok" "tld-!cn" "twitch" "hbo" "twitter" "youtube" "threads" "nvidia" "spotify")
 
@@ -32,4 +43,6 @@ for item in *.json; do
     mv "$item" rule-set/
 done
 
+# 删除临时文件
+echo "清理临时文件..."
 rm -rf sing-box.tar.gz sing-box-${VERSION}-linux-amd64/ geoip.db geosite.db sing-box
